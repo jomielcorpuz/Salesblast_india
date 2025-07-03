12abf02595981333fc41e482b829d4dfa3be3e4e
Clyde Suizo <141197487+clydeskieme1@users.noreply.github.com>
2025-07-03 21:55:25 +0800

    Gif

--- .history/app/layout_20250703070105.tsx
+++ .history/app/layout_20250703070105.tsx
@@ -15,8 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Salesblast India",
-  description:
-    "Connect with your customers like never before with Salesblast India, the ultimate sales and marketing platform designed to help you grow your business.",
+  description: "Connect with your customers like never before with Salesblast India, the ultimate sales and marketing platform designed to help you grow your business.",
   icons: {
     icon: "/public/icons/favicon-32x32.png", // Make sure your favicon file is in the 'public' folder
   },
@@ -32,9 +31,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute="class" defaultTheme="system">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+        >
           {children}
         </ThemeProvider>
+
       </body>
     </html>
   );

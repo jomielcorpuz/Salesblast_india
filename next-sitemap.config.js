806a095d0132fae4f9b0f56e2a91d7a7b2110086
mielcorpuz <jomiel.banog2x@gmail.com>
2025-07-16 06:21:06 +0800

    installed next-sitemap, added robots txt.

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.salesblastindia.com',
+  generateRobotsTxt: true, // Automatically creates robots.txt too
+  sitemapSize: 7000,
+}
